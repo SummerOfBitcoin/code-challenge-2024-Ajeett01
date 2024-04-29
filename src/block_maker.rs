4ba7c748ab1adc46dc769fa402cc85f254f24dfe
Ajeett01 <abhitiwari501@gmail.com>
2024-04-29 21:01:56 +0530

    path

--- src/block_maker.rs
+++ src/block_maker.rs
@@ -253,7 +253,7 @@ fn create_merkle_root(transactions: &Vec<String>) -> String {
 }
 
 fn read_trasactions() -> Vec<String> {
-    let path = "../mempool";
+    let path = "./mempool";
     let directory = fs::read_dir(path).unwrap();
 
     let mut transactions: Vec<String> = vec![];
